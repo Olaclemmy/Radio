1a9b7f71963b5c2b56cd6919110eeb030b5d93f4
toto <toto@dev.null.com>
2018-11-26 08:04:15 +0100

    add license

--- app/src/main/java/org/oucho/radio2/utils/ApkUtils.java
+++ app/src/main/java/org/oucho/radio2/utils/ApkUtils.java
@@ -1,3 +1,22 @@
+/*
+ * Radio - Internet radio for android
+ * Copyright (C) 2017  Old-Geek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.oucho.radio2.utils;
 
 import android.content.Context;
